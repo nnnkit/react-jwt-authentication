dd378237b1408988a42a6508019a1784d9073afa
nnnkit <iankit17@gmail.com>
2019-07-26 16:08:14 +0530

    remove content form read me file

--- src/components/Public.js
+++ src/components/Public.js
@@ -8,7 +8,7 @@ export default class Public extends Component {
   render() {
     return (
       <Switch>
-        <Route path="/signup" exact component={Signup} />
+        <Route path="/signup" component={Signup} />
         <Route path="/login" component={Login} />
         <Route component={FourNotFour} />
       </Switch>
